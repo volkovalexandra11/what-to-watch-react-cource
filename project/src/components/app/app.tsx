fcee6577247ca916ebe0c45ec571447bfecf771b
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-10-01 00:02:50 +0500

    Created proj

---
+++ project/src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import { Main } from '../../pages/main';
+
+function App(): JSX.Element {
+  return (
+    <Main
+      promoMovieName={'The Grand Budapest Hotel'}
+      promoMovieCreationDate={'2014'}
+      promoMovieGenre={'Drama'}
+    />);
+}
+
+export default App;
