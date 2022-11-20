7218e4a4147268463e97f6047c20687e1143b693
Alexandra Volkova <alexandr.sazhev@yandex.ru>
2022-11-20 23:32:15 +0500

    Fixes after review

--- project/src/components/main-page/card/card.tsx
+++ project/src/components/main-page/card/card.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import {TMovie} from '../../../types/TMovie';
+import { TMovie } from '../../../types/TMovie';
 
 type Props = {
-movie: TMovie;
+  movie: TMovie;
 }
 
 export const MovieCard: React.FC<Props> = ({ movie }) => (
   <article className="small-film-card catalog__films-card">
     <div className="small-film-card__image">
-      <img src={`/${movie.previewImage}`}
-        alt={movie.name} width="280" height="175"
-      />
+      <img src={`/${movie.previewImage}`} alt={movie.name} width="280" height="175"/>
     </div>
     <h3 className="small-film-card__title">
       <a className="small-film-card__link" href="/">{movie.name}</a>
